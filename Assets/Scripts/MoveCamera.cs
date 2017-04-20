0d8285eb642cec67702e5ff4529166c9f9558b52
Robin ten Buuren <robin.tenbuuren@nedap.com>
2017-04-20 18:17:58 +0200

    Remove rotating function from MoveCamera script

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -10,13 +10,11 @@ public class MoveCamera : MonoBehaviour
 	// VARIABLES
 	//
 
-	public float turnSpeed = 4.0f;		// Speed of camera turning when mouse moves in along an axis
 	public float panSpeed = 4.0f;		// Speed of the camera when being panned
 	public float zoomSpeed = 4.0f;		// Speed of the camera going back and forth
 
 	private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
 	private bool isPanning;		// Is the camera being panned?
-	private bool isRotating;	// Is the camera being rotated?
 	private bool isZooming;		// Is the camera zooming?
 
 	//
@@ -25,16 +23,8 @@ public class MoveCamera : MonoBehaviour
 
 	void Update () 
 	{
-		// Get the left mouse button
-		if(Input.GetMouseButtonDown(0))
-		{
-			// Get mouse origin
-			//mouseOrigin = Input.mousePosition;
-			//isRotating = true;
-		}
-
 		// Get the right mouse button
-		if(Input.GetMouseButtonDown(1))
+		if(Input.GetMouseButtonDown(0))
 		{
 			// Get mouse origin
 			mouseOrigin = Input.mousePosition;
@@ -42,7 +32,7 @@ public class MoveCamera : MonoBehaviour
 		}
 
 		// Get the middle mouse button
-		if(Input.GetMouseButtonDown(2))
+		if(Input.GetMouseButtonDown(1))
 		{
 			// Get mouse origin
 			mouseOrigin = Input.mousePosition;
@@ -50,18 +40,8 @@ public class MoveCamera : MonoBehaviour
 		}
 
 		// Disable movements on button release
-		if (!Input.GetMouseButton(0)) isRotating=false;
 		if (!Input.GetMouseButton(1)) isPanning=false;
 		if (!Input.GetMouseButton(2)) isZooming=false;
-
-		// Rotate camera along X and Y axis
-		if (isRotating)
-		{
-			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-
-			transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
-			transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
-		}
 
 		// Move the camera on it's XY plane
 		if (isPanning)
